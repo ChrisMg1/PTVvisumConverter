a6b822b25738c545a451dfc442d9e97695ece826
ChrisMg1_local_home <christoph.maget@outlook.com>
2021-02-26 11:11:15 +0100

    find doublettes

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import matplotlib
 
-path = 'C:/Users/blue/Downloads/HalteEinAus.att'
+# path = 'C:/Users/blue/Downloads/HalteEinAus.att'
+path = 'C:/Users/blue/Downloads/fpf_roh.att'
 
 
 def attribut2dataframe(attfile):
-    ret_att = pd.read_csv(attfile, skiprows=44, sep=';')
+    ret_att = pd.read_csv(attfile, skiprows=37, sep=';')
     # print(attfile)
     return ret_att
 
+def removeDoubleTimetable(timeDF):
+    find_doubles = timeDF.duplicated(subset=['DEP','LINENAME','DIRECTIONCODE'], keep='first')
+    timeDF['CM_DELETE'] = find_doubles.astype(int)
+    return timeDF
 
 work_thing = attribut2dataframe(path)
 
 for col in work_thing:
     print(col)
 
+removeDoubleTimetable(work_thing).to_csv(path_or_buf='C:/Users/blue/Downloads/fpf_witthDEL.att', sep=';',index=False)
 
-ax = work_thing.nlargest(20, 'PASSBOARD_TSYS(ICE,AP)').plot.bar(x='NAME', y='PASSBOARD_TSYS(ICE,AP)', rot=70)
-
-# df = pd.DataFrame({'lab':['A', 'B', 'C'], 'val':[10, 30, 20]})
-# ax = df.plot.bar(x='lab', y='val', rot=0)
-# matplotlib.pyplot.show()
-
-matplotlib.pyplot.savefig('out.png')
+# ax = work_thing.nlargest(20, 'PASSBOARD_TSYS(ICE,AP)').plot.bar(x='NAME', y='PASSBOARD_TSYS(ICE,AP)', rot=70)
+# matplotlib.pyplot.savefig('out.png')
